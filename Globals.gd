ca2651155c338d9b178044b3aac21dcdc691d5cf
unknown <dnadason@uco.edu>
2020-02-25 14:55:42 -0600

    Popup menu will close and open when user presses x ~ Luna

--- Globals.gd
+++ Globals.gd
@@ -1,8 +1,6 @@
 extends Node
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# Declare member variables here.
 const POPUP_SCENE = preload("res://pausePopup.tscn")
 const WORLD_PATH = "res://world.tscn"
 
@@ -11,28 +9,18 @@ var canvas_layer = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	canvas_layer = CanvasLayer.new()
-	add_child(canvas_layer)
-
-
-func load_new_scene(new_scene_path):
-     get_tree().change_scene(new_scene_path)
-	
-func _process(delta):
-	if Input.is_action_just_pressed("pause"):
-		popup_closed()
-		print("hold on")
-		popup = POPUP_SCENE.instance()
+    canvas_layer = CanvasLayer.new()
+    add_child(canvas_layer)
+    
+func _input(event):
+	if Input.is_action_pressed("pause"):
 		
-		canvas_layer.add_child(popup)
-		popup.popup()
-		get_tree().paused = true
-		
-			
-			
-func popup_closed():
-    get_tree().paused = false
+		if popup==null:
+			popup = POPUP_SCENE.instance() 
+			canvas_layer.add_child(popup)
+			popup.show()
 
-    if popup != null:
-        popup.queue_free()
-        popup = null
+		elif Input.is_action_pressed("pause") && popup!= null:
+			popup.queue_free()
+			popup=null
+
