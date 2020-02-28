4172cb12dc856998da9d8fcb2f1ca79031569d0a
Aram <acho@uco.edu>
2020-02-28 17:35:33 -0600

    Aram's Update

---
+++ Door.gd
@@ -0,0 +1,16 @@
+#Door.gd
+
+extends Area2D
+
+
+
+export(String, FILE, "*,tscn") var world_scene
+
+func _physics_process(delta):
+	var bodies = get_overlapping_bodies()
+	for body in bodies:
+		if body.name == "MC_Sprite":
+			if Input.is_action_just_pressed("enter_door"):
+			#go to next level
+				get_tree().change_scene(world_scene)
+				pass
--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,11 +1,16 @@
 extends KinematicBody2D
 
-const GRAVITY = 20
+#var door
+
+const GRAVITY = 10
 const SPEED = 100
 const JUMP = 500
 const RESISTANCE = Vector2(0,-1)
 
 var motion = Vector2()
+
+#func _ready():
+#	door = get_parent().get_node("Door")
 
 func _physics_process(delta):
 	#motion.y += GRAVITY
@@ -24,4 +29,14 @@ func _physics_process(delta):
 	
 	move_and_slide(motion,RESISTANCE)
 	
+
+#func _on_Door_area_entered(area):
+#	print("player entered the area")
+#	door.cango = true
+#	pass # replace with function body
+	
+#func _on_Door_area_exited(area):
+#	door.cango = false
+#	print("Player exited the area")
+#	pass # Replace with function body
 
