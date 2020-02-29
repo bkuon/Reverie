a644c5b17ab5bc7697e1d1a440bc409db2457e22
Cierra Gregg <dnadason@uco.edu>
2020-02-28 19:45:57 -0600

    fixed door

--- pausePopup.gd
+++ pausePopup.gd
@@ -7,7 +7,13 @@ func _input(event):
 	if event.is_action_pressed("pause"):
 		get_tree().paused=false
 
+func _on_save_btn_pressed():
+	print("Save game")
 
-	
-		
-	
+
+func _on_items_btn_pressed():
+	print("items menu")
+
+
+func _on_quit_btn_pressed():
+	print("quit game")
