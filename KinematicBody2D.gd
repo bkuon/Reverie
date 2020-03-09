8d4473647440c8da4d7387bb51d05ee2bc258b76
unknown <dnadason@uco.edu>
2020-03-09 16:57:07 -0500

    Added some animations, changed some textures, MC will no longer walk off screen and fall

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,55 +1,49 @@
 extends KinematicBody2D
 
-const GRAVITY = 20
-const SPEED = 100
-const JUMP = 500
-const RESISTANCE = Vector2(0,-1)
-
 var motion = Vector2()
-var obj_name: String
-var obj = null
+
+const UP = Vector2(0,-1)
+const GRAVITY = 20
+const ACCELERATION = 10
+const MAX_SPEED = 300
+const JUMP_HEIGHT = -500
+export var speed = 10.0
+
 
 func _physics_process(delta):
-	#motion.y += GRAVITY
+	
+	motion.y += GRAVITY
+	var friction = false 
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x= SPEED
-		$sprite.flip_h = false
-		$sprite.play("walk")
+		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
+		$Sprite.flip_h = false
+		$Sprite.play("walk")
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -SPEED
-		$sprite.flip_h = true
-		$sprite.play("walk")
-	elif Input.is_action_just_pressed("interact"):
-		if obj and obj_name != "" and obj_name != "Door" and obj.can_speak:
-			get_node("../DialogueParser").init_dialogue(obj_name)
-	#elif is_on_floor():
-		#if Input.is_action_just_pressed("ui_up")
-			#motion.y = JUMP
-		#else:
-			#$sprite.play("jump")
+		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
+		$Sprite.flip_h = true
+		$Sprite.play("walk")	
 	else:
-		motion.x=0
-		$sprite.play("idle")
+		$Sprite.play("idle")
+		friction = true 
 	
-	if Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_right"):
-		motion.x=0
-		$sprite.play("idle")
+	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
+		motion.x = 0
+		$Sprite.play("idle")
 	
-	move_and_slide(motion,RESISTANCE)
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = JUMP_HEIGHT
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.2)
+	else:
+		if motion.y < 0: 
+			$Sprite.play("Jump")
+		else:
+			$Sprite.play("Fall")
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.05)
+	motion = move_and_slide(motion, UP)
 	
-
-#signals when player is touching object. 
-#gets object name for DialogueParser
-func _on_Area2D_area_entered(area):
-	obj_name = area.name
-	obj = area.get_parent()
+	pass
 	
-	
-func _on_Area2D_area_exited(area):
-	obj = ""
-	
-# custom signal says that the item has been checked for/ or has finished dialogue
-func _on_DialogueParser_done_talking():
-	obj.can_interact = true
-	obj.can_speak = false
