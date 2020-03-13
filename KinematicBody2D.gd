faeca0eb2293708973f3724a1b1033c62e9c40a9
Brandon Kuon <bkuon@uco.edu>
2020-03-13 14:57:50 -0500

    Duck physics and funcitons implemented, alongside added jump ability.

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -2,11 +2,16 @@ extends KinematicBody2D
 
 #Added variables for Jump
 const UP = Vector2(0, -1)
-const JUMP_HEIGHT = -550
+const JUMP_HEIGHT = -200
+#Brandon 3/12 
 
 #Added Variables for Run
 const ACCELERATION = 50
-const MAX_SPEED = 300
+const MAX_SPEED = 200
+#Brandon 3/12
+
+#Duck Speed
+const DUCK_SPEED = 50
 
 export var speed = 10.0 
 
@@ -33,11 +38,11 @@ func _physics_process(delta):
 	elif Input.is_action_just_pressed("interact"):
 		if obj and obj_name != "" and obj_name != "Door" and obj.can_speak:
 			get_node("../DialogueParser").init_dialogue(obj_name)
-	#elif is_on_floor():
-		#if Input.is_action_just_pressed("ui_up")
-			#motion.y = JUMP
-		#else:
-			#$sprite.play("jump")
+	elif is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = JUMP_HEIGHT
+		else:
+			$sprite.play("jump")
 	else:
 		motion.x=0
 		$sprite.play("idle")
@@ -46,15 +51,63 @@ func _physics_process(delta):
 		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
 		$sprite.flip_h = false
 		$sprite.play("run")
+		if is_on_floor():
+			if Input.is_action_just_pressed("ui_up"):
+				motion.y = JUMP_HEIGHT
+			if friction == true:
+				motion.x = lerp(motion.x, 0, 0.2)
+		else:
+			if motion.y < 0: 
+				$sprite.play("Jump")
+			else:
+				$sprite.play("Fall")
+			if friction == true:
+				motion.x = lerp(motion.x, 0, 0.05)
+			
+			
+		motion = move_and_slide(motion, UP)
+	
+		move_and_slide(motion,RESISTANCE)
 	if Input.is_action_pressed("ui_left") and Input.is_key_pressed(KEY_V):
 		motion.x = min(motion.x - ACCELERATION, MAX_SPEED)
 		$sprite.flip_h = true
 		$sprite.play("run")
+		
+		if is_on_floor():
+			if Input.is_action_just_pressed("ui_up"):
+				motion.y = JUMP_HEIGHT
+			if friction == true:
+				motion.x = lerp(motion.x, 0, 0.2)
+		else:
+			if motion.y < 0: 
+				$sprite.play("Jump")
+			else:
+				$sprite.play("Fall")
+			if friction == true:
+				motion.x = lerp(motion.x, 0, 0.05)
+			
+			
+		motion = move_and_slide(motion, UP)
+	
+		move_and_slide(motion,RESISTANCE)
+		
 	
 	
 	if Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_right"):
 		motion.x=0
 		$sprite.play("idle")
+	
+	#Ability to duck here
+	if Input.is_action_pressed("ui_down"):
+		#$sprite.play("duck")
+		motion.x = 0
+	if Input.is_action_pressed("ui_down") and Input.is_action_pressed("ui_right"):
+		#$sprite.play("duck")
+		motion.x = DUCK_SPEED
+	if Input.is_action_pressed("ui_down") and Input.is_action_pressed("ui_left"):
+		#$sprite.flip_h = true
+		#$sprite.play("duck")
+		motion.x = -DUCK_SPEED
 	
 	#Ability to jump implemented here
 	if is_on_floor():
@@ -69,6 +122,8 @@ func _physics_process(delta):
 			$sprite.play("Fall")
 		if friction == true:
 			motion.x = lerp(motion.x, 0, 0.05)
+			
+			
 	motion = move_and_slide(motion, UP)
 	
 	move_and_slide(motion,RESISTANCE)
