9fe82cbdd3f32b29ad263ae8e094dadbc6db3227
Cierra Gregg <cgregg5@uco.edu>
2020-03-14 14:02:02 -0500

    Patrick code

--- Coin.gd
+++ Coin.gd
@@ -5,6 +5,8 @@ onready var globals = get_node("/root/Globals")
 var can_speak = false
 var can_interact = true
 
+signal coinCollected
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
@@ -13,10 +15,15 @@ func _process(delta):
 		if b.name == "MC_Sprite":
 			print("Picked up Coin")
 			globals.coinCounter += 1
+			emit_signal("coinCollected")
+			$Coin/AudioStreamPlayer.play()
 			if(globals.coinCounter == 1):
 				print("You have " + str(globals.coinCounter) + " coin.")
 			elif(globals.coinCounter > 1):
 				print("You have " + str(globals.coinCounter) + " coins.")
-			queue_free()
+			$Coin/CollisionShape2D.disabled = true
+			$Coin/Sprite.visible = false
+			#queue_free()
 	
 	pass
+
