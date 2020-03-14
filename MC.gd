8b1163ef241592c77054e82d10b3f151fa13af22
unknown <dnadason@uco.edu>
2020-03-14 15:49:35 -0500

    Components for gaining new powers added

--- MC.gd
+++ MC.gd
@@ -10,10 +10,19 @@ const JUMP_HEIGHT = -500
 export var speed = 10.0
 
 
+
 func _physics_process(delta):
+		
+	if MC_Globals.canJump:
+		$CanvasLayer/jumpIcon.visible=true
+	if MC_Globals.canRun:
+		$CanvasLayer/runIcon.visible=true
+	if MC_Globals.canCrawl:
+		$CanvasLayer/jumpCrawl.visible=true
 	
 	motion.y += GRAVITY
 	var friction = false 
+	$CanvasLayer/jumpIcon.play("default")
 	
 	if Input.is_action_pressed("ui_right"):
 		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
@@ -32,18 +41,34 @@ func _physics_process(delta):
 		$Sprite.play("idle")
 	
 	if is_on_floor():
-		if Input.is_action_just_pressed("ui_up"):
+		if Input.is_action_just_pressed("ui_up") && MC_Globals.canJump:
 			motion.y = JUMP_HEIGHT
+			MC_Globals.isJumping=true
+				
 		if friction == true:
 			motion.x = lerp(motion.x, 0, 0.2)
 	else:
 		if motion.y < 0: 
 			$Sprite.play("Jump")
+			$CanvasLayer/jumpIcon.play("active")
 		else:
 			$Sprite.play("Fall")
+			MC_Globals.isJumping=false
 		if friction == true:
 			motion.x = lerp(motion.x, 0, 0.05)
+			
+	if MC_Globals.victory:
+		$Sprite.play("victory")
+		_on_Sprite_animation_finished()
+	
+		
 	motion = move_and_slide(motion, UP)
+	
+	
 	
 	pass
 	
+
+
+func _on_Sprite_animation_finished():
+	 MC_Globals.victory=false
